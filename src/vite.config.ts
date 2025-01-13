dbbcb47aa07bb82aa91c266db95bc63c9940f45b
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-01-13 02:06:01 +0100

    chore: #1 Created basic project structure and set up absolute paths

---
+++ src/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/features"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+    },
+  },
+});
